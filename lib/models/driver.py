075b97f10d7cb8b77938d7d65765b4861a6d8756
Lisa Alusa <sayilisaalusa@gmail.com>
2025-05-27 12:52:51 +0300

    completed driver file

--- lib/models/driver.py
+++ lib/models/driver.py
@@ -6,4 +6,55 @@ class Driver:
         self.name = name
         self.nationality = nationality
 
-        def 
+        def save(self):
+            conn = get_connection()
+            cursor = conn.cursor()
+            try:
+                if self.id:
+                    cursor.execute(
+                        "UPDATE drivers SET name=?, nationality=? WHERE id=?",
+                        (self.name, self.nationality, self.id)
+                    )
+                    else:
+                    cursor.execute(
+                        "INSERT INTO drivers (name, nationality) VALUES (?, ?)",
+                        (self.name, self.nationality)
+                    )
+                    self.id = cursor.lastrowid
+                    conn.commit()
+                    finally:
+conn.close()
+
+@classmethod
+def find_by_id(cls, driver_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM drivers WHERE id=?", (driver_id,))
+    row = cursor.fetchone()
+    conn.close()
+    if row:
+        return cls(name=row['name'], nationality=row['nationality'], id=row['id'])
+    return None
+
+def races(self):
+    """Get all races for this driver."""
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+SELECT r.* FROM races r
+                   JOIN results res ON r.id = res.race_id
+                   WHERE res.driver_id = ?
+                   """, (self.id,))
+    rows = cursor.fetchall()
+    def add_race_result(self, team_id, race_id, position, points):
+        """Adding a new race result for this driver."""
+    conn = get_connection()
+    cursor = conn.cursor()
+    try:
+        cursor.execute("""
+                       INSERT INTO results (driver_id, team_id, race_id, position, points))
+                          VALUES (?, ?, ?, ?, ?)
+                          """, (self.id, team_id, race_id, position, points))
+        conn.commit()
+        finally:
+        conn.close()
